8360c91f84a5f752c5754d06a5aabf6dfb16dd48
Benedikt Magnus <magnus@magnuscraft.de>
2022-05-25 13:59:01 +0200

    Renamed Class "Plugin" to "SharedWatchedStatusPlugin".
    This prevents a collision with the namespace.

--- Jellyfin.Plugin.SharedWatchedStatus/SharedWatchedStatusPlugin.cs
+++ Jellyfin.Plugin.SharedWatchedStatus/SharedWatchedStatusPlugin.cs
@@ -10,7 +10,7 @@ namespace Jellyfin.Plugin.SharedWatchedStatus
     /// <summary>
     /// The main plugin.
     /// </summary>
-    public class Plugin: BasePlugin<Configuration.PluginConfiguration>, IHasWebPages
+    public class SharedWatchedStatusPlugin: BasePlugin<Configuration.PluginConfiguration>, IHasWebPages
     {
         /// <inheritdoc/>
         public override string Name => "Shared Watched Status";
@@ -18,12 +18,15 @@ namespace Jellyfin.Plugin.SharedWatchedStatus
         /// <inheritdoc/>
         public override Guid Id => Guid.Parse("af7e610f-ed05-4efd-bd69-c2269cf701f2");
 
-        public static Plugin Instance { get; private set; }
+        public static SharedWatchedStatusPlugin Instance { get; private set; }
 
         /// <summary>
         /// Initialises a new instance for the plugin system.
         /// </summary>
-        public Plugin (IApplicationPaths applicationPaths, IXmlSerializer xmlSerialiser): base(applicationPaths, xmlSerialiser)
+        public SharedWatchedStatusPlugin (
+            IApplicationPaths applicationPaths,
+            IXmlSerializer xmlSerialiser
+        ): base(applicationPaths, xmlSerialiser)
         {
             Instance = this;
         }
